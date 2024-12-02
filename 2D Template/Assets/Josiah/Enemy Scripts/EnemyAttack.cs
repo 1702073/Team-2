35f92cd4bf52a00d8605156c3ca6ce5da672afd2
MUKILTEO\1721887 <1721887@mukilteo.wednet.edu>
2024-12-02 13:35:59 -0800

    Added attack cooldown

--- 2D Template/Assets/Josiah/Enemy Scripts/EnemyAttack.cs
+++ 2D Template/Assets/Josiah/Enemy Scripts/EnemyAttack.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
@@ -5,22 +6,26 @@ using UnityEngine;
 
 public class EnemyAttack : MonoBehaviour
 {
-    public int health = 3;
+    public int health;
 
     public LayerMask AttackLayer;
+
+    public float cooldown;
+    public bool attackReady = true;
 
     private void Update()
     {
         // If the distance between the enemy and the player is less than or equal to one, then it will attack
-        if (Vector2.Distance(transform.position, GameObject.FindGameObjectWithTag("Player").transform.position) <= 1)
+        if ((Vector2.Distance(transform.position, GameObject.FindGameObjectWithTag("Player").transform.position) <= 1) && (attackReady == true))
         {
             Attack();
         }
+
     }
 
     public void Attack()
     {
-        //Spawn a circle hitbox and sees what it hit
+        // Spawn a circle hitbox and sees what it hit
         var hit = Physics2D.CircleCast(transform.position + Vector3.up, 1, Vector2.zero, 0, AttackLayer);
 
         // See if it hit the player
@@ -29,5 +34,16 @@ public class EnemyAttack : MonoBehaviour
             // Attacks the player
             hit.collider.GetComponent<PlayerTest>().health -= 1;
         }
+        
+        attackReady = false;
+
+        // Forces the enemy to wait before it can attack again
+        Invoke(nameof(ResetAttack), cooldown);
+    }
+
+
+    private void ResetAttack()
+    {
+        attackReady = true;
     }
 }
