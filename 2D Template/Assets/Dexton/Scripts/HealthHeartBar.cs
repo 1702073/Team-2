d73e6759f2509799a4109f79e8c21d447a61f451
MUKILTEO\1702073 <1702073@mukilteo.wednet.edu>
2024-12-03 11:21:29 -0800

    Changes to heart system

---
+++ 2D Template/Assets/Dexton/Scripts/HealthHeartBar.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering;
+
+public class HealthHeartBar : MonoBehaviour
+{
+    public GameObject heartPrefab;
+    public float health, maxHealth;
+    public PlayerHealth playerHealth;
+    List<HealthHeart> hearts = new List<HealthHeart>();
+
+    public void DrawHearts()
+    {
+        ClearHearts();
+
+        //Determine how many hearts to make total
+        //Based on max health
+        float maxHealthRemainder = playerHealth.maxHealth;
+    }
+
+    public void CreateEmptyHearts()
+    {
+        GameObject newHeart = Instantiate(heartPrefab);
+        newHeart.transform.SetParent(transform);
+
+        HealthHeart heartComponent = newHeart.GetComponent<HealthHeart>();
+        heartComponent.SetHeartImage(HeartStatus.Empty);
+        hearts.Add(heartComponent);
+    }
+
+    public void ClearHearts()
+    {
+        foreach (Transform t in transform)
+        {
+            Destroy(t.gameObject);
+        }
+        hearts = new List<HealthHeart> ();
+    }
+}
