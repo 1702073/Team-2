706c258a6237bd6423ad27ec436a1355f47d54aa
MUKILTEO\1702073 <1702073@mukilteo.wednet.edu>
2025-01-15 14:56:40 -0800

    add salt attack and item use start bottle use

--- 2D Template/Assets/Dexton/Scripts/Player Scripts/Salt_behavior.cs
+++ 2D Template/Assets/Dexton/Scripts/Player Scripts/Salt_behavior.cs
@@ -6,23 +6,15 @@ public class Salt_behavior : MonoBehaviour
 {
     public LayerMask EnemyLayer;
     public float radius;
-    public float freezeTime;
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
     // Update is called once per frame
     void Update()
     {
-        var hit = Physics2D.CircleCast(transform.position, radius, Vector2.zero, 0, EnemyLayer);
-        if (hit && hit.collider.CompareTag("Enemy"))
+        var hits = Physics2D.CircleCastAll(transform.position, radius, Vector2.zero, 0, EnemyLayer);
+        foreach (RaycastHit2D hit in hits)
         {
-            // Attacks the enemy
-            //hit.collider.GetComponent<EnemyGeneral>().EnemyTakeDamage(1);
-            //hit.rigidbody.AddForce(GetAttackDirection(), ForceMode2D.Impulse);
+            Debug.Log("djhfkshdufsdhf");
+            hit.collider.GetComponent<EnemyFollow>().EnemyFreeze();
         }
     }
 }
--- 2D Template/Assets/Josiah/Scripts/Enemy Scripts/EnemyFollow.cs
+++ 2D Template/Assets/Josiah/Scripts/Enemy Scripts/EnemyFollow.cs
@@ -8,6 +8,9 @@ public class EnemyFollow : MonoBehaviour
     public float stoppingDistance;
     public Animator enemyAnimator;
 
+    public float effectTime;
+    private float baseSpeedStored;
+
     Vector2 movement;
 
     private Transform target;
@@ -16,15 +19,25 @@ public class EnemyFollow : MonoBehaviour
 
     private Rigidbody2D rb2D;
 
+    public bool isStunned;
+    public bool isSlowed;
+
     private void Start()
     {
         rb2D = GetComponent<Rigidbody2D>();
         enemyGeneral = GetComponent<EnemyGeneral>();
         target = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+        baseSpeedStored = speed;
     }
 
     private void Update()
     {
+        if (isStunned == true)
+        {
+            effectTime -= Time.deltaTime;
+            EnemyFreeze();
+        }
+
         switch (enemyGeneral.state)
         {
             case EnemyGeneral.State.Idle:
@@ -44,6 +57,42 @@ public class EnemyFollow : MonoBehaviour
                 enemyAnimator.SetFloat("Speed", movement.sqrMagnitude);
 
                 break;
+        }
+    }
+
+    public void EnemyFreeze()
+    {
+        isStunned = true;
+
+        speed = 0;
+        Invoke(nameof(ResetEnemySpeed), 3);
+    }
+    public void EnemySlow()
+    {
+        isStunned = true;
+
+        speed = baseSpeedStored * 0.5f;
+        Invoke(nameof(ResetEnemySpeed), 3);
+    }
+
+    public void ResetEnemySpeed()
+    {
+        Debug.Log("not brrrr");
+        isStunned = false;
+        speed = baseSpeedStored;
+    }
+
+    public void EnemySlow()
+    {
+        isSlowed = true;
+        if (effectTime > 0)
+        {
+            speed = baseSpeedStored * 0.5f;
+        }
+        if (effectTime <= 0)
+        {
+            isSlowed = false;
+            speed = baseSpeedStored;
         }
     }
 }
